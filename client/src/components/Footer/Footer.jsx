8c71071aeb3ee41c22069b8043b54430a7bc084b
hdd <comehere.hdd@gmail.com>
2024-04-23 00:58:00 +0700

    modify

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -81,7 +81,7 @@ const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="pb-16 pt-10 bg-[#001431]">
+    <footer className="pb-16 pt-10">
       <div className="container">
         <div className="flex justify-between flex-col md:flex-row flex-wrap gap-[30px]">
           <div>
