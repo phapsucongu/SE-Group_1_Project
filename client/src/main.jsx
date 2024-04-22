80d3c652e4d05b97e9b109bef164c2e4091a4dc3
phapsucongu <hatiendong2004@gmail.com>
2024-04-23 00:52:18 +0700

    nah

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import AuthContextProvider from './context/AuthContext.jsx';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { AuthContextProvider } from './context/AuthContext.jsx';
+//import { AuthContextProvider } from './context/AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
